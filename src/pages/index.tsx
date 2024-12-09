b3a4f7a5d82257cdc42b63641e2e259aa29ae188
a74523 <a74523@ualg.pt>
2024-12-09 15:31:39 +0000

    eliminaçao do link na pag principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,6 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog/Authors">
-            Authors️
-          </Link>
         </div>
       </div>
     </header>
