627355eb21cbd4405019eb838b6ca30cb54e0188
a74523 <a74523@ualg.pt>
2024-12-09 11:27:23 +0000

    Alteração do link do git

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/duarteesteves97/tp3',
             label: 'GitHub',
             position: 'right',
           },
